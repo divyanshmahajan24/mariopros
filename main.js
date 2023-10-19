0c6a98a5fcd523748caaca2c76a830793e4d40a9
Divyansh Mahajan <divyanshmahajan@IT001636.local>
2023-10-20 01:35:22 +0400

    add routing

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
+let http = require('http');
 const express = require ('express');
 const app = express ();
+let router = require('./routes');
 
 const server = require ('http').createServer (app);
 
 app.set('views', __dirname + '/views');
 app.engine('.html', require('ejs').__express);
 
-app.get ('/', function (req, res){
-    res.render('index.html');    
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
     });
 
-server.listen (8000);
+};
+
+http.createServer(router.handleRequest).listen(8000);
